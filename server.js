b1f979a2553fa06d6f4ff1820c43f3aa905b3d7d
Jchilies <paddleroyale@proton.me>
2026-04-06 13:39:52 -0700

    Fix x402Express destructured import

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const x402Express = require('x402-express');
+const { x402Express } = require('x402-express');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
